f80cdb1e2b16e45fbb555b41a209981d9ddd92ac
cobanberfin <73108874+cobanberfin@users.noreply.github.com>
2021-02-14 01:39:36 +0300

    add to file

---
+++ projemiz/projemiz/face-recognition.py
@@ -0,0 +1,90 @@
+import face_recognition
+import cv2
+import numpy as np
+
+video_capture = cv2.VideoCapture(0)
+
+belgin_image = face_recognition.load_image_file("belgin.jpg")
+belgin_face_encoding = face_recognition.face_encodings(belgin_image)[0]
+
+
+berfin_image = face_recognition.load_image_file("berfin.jpg")
+berfin_face_encoding = face_recognition.face_encodings(berfin_image)[0]
+
+erkan_image = face_recognition.load_image_file("erkan.jpg")
+erkan_face_encoding = face_recognition.face_encodings(erkan_image)[0]
+
+
+elif_image = face_recognition.load_image_file("elif.jpg")
+elif_face_encoding = face_recognition.face_encodings(elif_image)[0]
+
+
+known_face_encodings = [
+    belgin_face_encoding,
+    berfin_face_encoding,
+    erkan_face_encoding,
+    elif_face_encoding
+]
+known_face_names = [
+    "Belgin coban",
+    "Berfin coban",
+    "erkan özhan",
+    "ElifNur Baş"
+]
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+
+    ret, frame = video_capture.read()
+
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    if process_this_frame:
+
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+video_capture.release()
+cv2.destroyAllWindows()
